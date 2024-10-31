ba48e410a44ae8035239b7da3d27569fd2c3873b
JTT900 <141504557+JTT900@users.noreply.github.com>
2024-10-31 08:04:08 +0100

    Fixed space error on line6

--- File-search.sh
+++ File-search.sh
@@ -3,8 +3,17 @@
 echo "Type a file name to see if it exists"
 read filename
 
+# Checks if argument passed, if not exits script
+if [[ $# -ne 1 ]]; then
+  echo "No arguments passed."
+  exit 1
+
+
+# Checks if file typed exists
 if [[ -f "$filename" ]] ; then
 	echo "$filename exists"
 else
 	echo "$filename does not exist"
 fi
+
+exit 0

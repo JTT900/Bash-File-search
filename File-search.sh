fba1a0ad3a158da1c58b01e9d87fcbd64332fdc4
JTT900 <141504557+JTT900@users.noreply.github.com>
2024-10-31 11:10:48 +0100

    Added function terminate to display argument error and exit.
    Added help section called by -h // --help

--- File-search.sh
+++ File-search.sh
@@ -2,15 +2,28 @@
 
 # Script name for usage message
 readonly SCRIPT_NAME=${0##*/}
+readonly CL_ARGS_ERROR=155
 
 # Displays usage help of script
 usage() {
   cat <<USAGE
 Usage: ${SCRIPT_NAME}
-Type a file name to see if it exists.
+
+This script lets user search for a file
+Arguments:
+  filename  The file to be searched.
+
+Options:
+  -h, --help  Shows this help message and exit
 USAGE
 }
 
+terminate(){
+  msg="${1}"
+  code="${2:-160}"
+  echo "Error ${msg}" >&2
+  exit "${code}"
+}
 # Prompts user to type file name
 echo "Type a file name to see if it exists"
 read -r filename
@@ -18,9 +31,13 @@ read -r filename
 # Checks if argument passed, if not exits script
 if [[ $# -ne 1 ]]; then
   usage
-  exit 1
+  terminate "Command line argument missing" "${CL_ARGS_ERROR}"
 fi
 
+if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
+  usage
+  exit 0
+fi
 
 # Checks if file typed exists
 if [[ -f "$filename" ]] ; then
